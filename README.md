6e856b1776907df3730d67340cd6574644cd91c1
Filipe Mendes <mendes.m.filipe@gmail.com>
2020-09-16 18:12:04 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Em construção :construction:
+
+<img src="https://media1.giphy.com/media/26ybvJNaZZKpPONEc/giphy.gif" width="480" height="270" />
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+export default () => {
+  return(
+  <div className="app-window">
+    <div className="sidebar">
+
+    </div>
+  </div>
+  );
+}
