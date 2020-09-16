3d2ef20f130d72ba47a853354759f1608bdb46ce
Filipe Mendes <mendes.m.filipe@gmail.com>
2020-09-16 20:27:29 -0300

    little adjustments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
 
-
 export default () => {
-  return(
-  <div className="app-window">
-    <div className="sidebar">
+  return (
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img
+            className="header--avatar"
+            src="https://lh3.googleusercontent.com/proxy/YMFeBxtDJEnz4Dpk0HBNQRtLMxDJpkXcpi84Q2x0VLqNe6xcfr9mnVxTemcBj-_X7oSElIpwCkHoJKJwpkxBJCXhuUOZ2C4mzFxfnlsvYcRO3Q"
+            alt="avatar"
+          />
+          <div className="header--buttons">
+            ...
+          </div>
+        </header>
 
+        <div className="search">...</div>
+
+        <div className="chatlist">...</div>
+      </div>
+      <div className="contentarea">...</div>
     </div>
-  </div>
   );
-}
+};
